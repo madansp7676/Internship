b19cbeb130ed1b95d3f6670cd2d675c6fe88f618
madansp7676 <madansettajana@gmail.com>
2026-02-20 11:02:04 +0530

    first commit

--- date_20_02.java
+++ date_20_02.java
@@ -1,14 +1,49 @@
-public class date_20_02 {
+// public class date_20_02 {
     
-        static void test(int a){
+//         static void test(int a) //formal argument
+//         {
             
-            System.out.println("Value: "+a);
-        }
+//             System.out.println("Value: "+a);
+//         }
     
-    public static void main(String[] args) {
+//     public static void main(String[] args) {
 
-        test(25);
-    }
+//         test(25); //actual argument
+//     }
         
+// }
+
+//program2:
+ class date_20_02
+ {
+//     static void disp(double d)
+//     {
+//         System.out.println("value " + d);
+//     }
+//     public static void main(String[] args)
+//     {
+//         disp(2.3);
+//         disp(32.31);
+//     }
+ 
+
+// output:value 2.3
+// value 32.31
+
+//program3:
+ static void run(boolean bool)
+ {
+        System.out.println("passedvalue " + bool);
+
+ }
+ static void help(char ch)
+ {
+    System.out.println("value passed  " + ch);
+    run(true);
+ }
+ public static void main(String[] args)
+ {
+    help('J');
+ }
 }
 
---
+++ methods/passing values/class program 1.java
@@ -0,0 +1,12 @@
+class program1
+{
+    static void disp(double d)
+    {
+        System.out.println("value " + d);
+    }
+    public static void main(String[] args)
+    {
+        disp(2.3);
+        disp(32.31);
+    }
+}
