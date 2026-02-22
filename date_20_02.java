e984a0a8f9366cc242da6b231ed89a178e75220a
madansp7676 <madansettajana@gmail.com>
2026-02-22 12:50:33 +0530

    passing method

--- date_20_02.java
+++ date_20_02.java
@@ -31,19 +31,93 @@
 // value 32.31
 
 //program3:
- static void run(boolean bool)
- {
-        System.out.println("passedvalue " + bool);
+//  static void run(boolean bool)
+//  {
+//         System.out.println("passedvalue " + bool);
+
+//  }
+//  static void help(char ch)
+//  {
+//     System.out.println("value passed  " + ch);
+//     run(true);
+//  }
+//  public static void main(String[] args)
+//  {
+//     help('J');
+//  }
+
+//  output:
+//     value passed  J
+//     passedvalue true
+
+// static void test(int a,int b)
+// {
+//     int sum=a+b;
+//     System.out.println(a+" + "+b+" = "+sum);
+
+// }
+// public static void main(String[] args)
+// {
+//     test(10,20);
+//     test(100,200);
+//     test(1000,2000);
+// }
+// output:
+// 10 + 20 = 30
+// 100 + 200 = 300
+// 1000 + 2000 = 3000
+
+
+//program6:
+
+// static void display(int a,char c,boolean b)
+// {System.out.println("Integer : "+a+", character : "+c+", boolean : "+b);
+
+// }
+// public static void main(String[] args)
+// {
+//     display(10,'A',true);
+// }
+
+// output:
+// Integer : 10, character : A, boolean : true
+
+// static int test()
+// {
+//     return 10;
+// }
+// public static void main(String[] args)
+// {
+//     int val=test();
+//     System.out.println("Value returned by test() : "+val);
+//     System.out.println("Value returned by test() : "+test());
+//  }
+
+// output:
+// Value returned by test() : 10
+// Value returned by test() : 10    
+
+
+//program6:
+
+// {
+//     static double play()
+//     {
+//         return 4.5;
+//     }
+//     public static void main(String[] args)
+//     {
+//         double d=play();
+//         System.out.println("Value returned by play() : "+d);
+//         System.out.println("Value returned by play() : "+play());
+//      }
+// }
+ 
+
+//rework
+
 
  }
- static void help(char ch)
- {
-    System.out.println("value passed  " + ch);
-    run(true);
- }
- public static void main(String[] args)
- {
-    help('J');
- }
-}
+
+
 
---
+++ demo.java
@@ -0,0 +1,10 @@
+class demo {
+
+    public static void Hello() {
+        System.out.println("Hello");
+    }
+
+    public static void main(String[] args) {
+        Hello();
+    }
+}
