81d0c87978be40329cfeb532a27fcaa243cc6c86
madansp7676 <madansettajana@gmail.com>
2026-02-24 21:43:02 +0530

    Add arrays folder files

---
+++ arrays/program1.java
@@ -0,0 +1,14 @@
+class program1{
+    public static void main(String[] args) {
+        int[] arr=new int[5];
+        arr[0]=14;
+        arr[1]=12;
+        arr[2]=10;
+        arr[3]=27;
+        arr[4]=76;
+    for(int i=0;i<arr.length;i++)
+        {
+            System.out.println(arr[i]);
+        }    
+    }
+}    
---
+++ arrays/program3.java
@@ -0,0 +1,10 @@
+class program3
+{
+    public static void main(String[] args) {
+        double[] dArr={8.4,6.6,7.4,7.8,4.5};
+        for(int i=0;i<dArr.length;i++)
+        {
+            System.out.println(dArr[i]);
+        }
+    }
+}
